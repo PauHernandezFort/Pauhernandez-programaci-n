b3ac7dae5fc777cb29a315e0b96155f90ef53be4
PauHernandezFort <146077995+PauHernandezFort@users.noreply.github.com>
2024-03-29 05:26:46 +0100

    casi todo hecho me queda el opcional

---
+++ 3Eval/www/av8/clases/Gestion.php
@@ -0,0 +1,39 @@
+<?php
+
+class Gestion extends Conexion{
+    public function __construct()
+    {
+        $this->fichero = 'costumers.csv';
+        parent::__construct();
+    }
+
+public function getBrands(){
+    $conn = $this->getConn();
+    $query="SELECT `brandId`,`brandName`FROM `brands` ";
+    $result = mysqli_query($conn, $query);
+    $mostar ="<!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.'>
+        <title>Document</title>
+        </head>
+        <body>
+        <form action='' method='post'>";
+    if($result){
+        while ($brand = $result->fetch_assoc()) {
+            $brandId = $brand['brandId'];
+            $brandName = $brand['brandName'];
+            $mostar.= " <input type='checkbox' value='$brandId' name='$brandName'> $brandName<br>";
+            }
+            $mostar.="  <input type='submit' value='Seleccionar'>
+            </form>
+        </body>
+        </html>";
+        }
+        echo $mostar;
+    }
+
+  }
+?>
